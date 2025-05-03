3579447ef66c19cd3cc9e695e7557c16940b5d99
Nikhil Karle <nikhilkarle@Nikhils-MacBook-Air.local>
2025-05-03 17:27:31 -0400

    added pagination and search

--- cmd/api/feed.go
+++ cmd/api/feed.go
@@ -1,10 +1,31 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/nikhilkarle/social/internal/store"
+)
 
 func (app *application) getUserFeedHandler(w http.ResponseWriter, r *http.Request){
 
-	feed, err := app.store.Posts.GetUserFeed(r.Context(), int64(51))
+	fq := store.PaginatedFeedQuery{
+		Limit: 20,
+		Offset: 0,
+		Sort: "desc",
+	}
+
+	fq, err := fq.Parse(r)
+	if err != nil{
+		app.badRequestError(w, r, err)
+		return
+	}
+
+	if err := Validate.Struct(fq); err != nil{
+		app.badRequestError(w, r, err)
+		return
+	}
+
+	feed, err := app.store.Posts.GetUserFeed(r.Context(), int64(50), fq)
 
 	if err != nil{
 		app.internalServerError(w, r, err)
